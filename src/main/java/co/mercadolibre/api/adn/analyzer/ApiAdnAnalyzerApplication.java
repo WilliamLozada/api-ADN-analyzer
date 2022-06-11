301e7c3275e5f4793b70d87da68373f79c0857c9
WilliamLozada <william@cobre.co>
2022-06-11 11:05:22 -0500

    Project start

---
+++ src/main/java/co/mercadolibre/api/adn/analyzer/ApiAdnAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package co.mercadolibre.api.adn.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAdnAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAdnAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/mercadolibre/api/adn/analyzer/ApiAdnAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.mercadolibre.api.adn.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAdnAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
