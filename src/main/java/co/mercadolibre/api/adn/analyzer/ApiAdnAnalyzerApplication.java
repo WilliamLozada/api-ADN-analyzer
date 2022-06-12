59331cfd0cb741c9dbcf10ca8b5ad98f97b8ca71
WilliamLozada <william@cobre.co>
2022-06-12 12:52:09 -0500

    Set banner

--- src/main/java/co/mercadolibre/api/adn/analyzer/ApiAdnAnalyzerApplication.java
+++ src/main/java/co/mercadolibre/api/adn/analyzer/ApiAdnAnalyzerApplication.java
@@ -1,13 +1,31 @@
 package co.mercadolibre.api.adn.analyzer;
 
+import java.io.PrintStream;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ApiAdnAnalyzerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiAdnAnalyzerApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication app = new SpringApplication(ApiAdnAnalyzerApplication.class);
+    app.setBanner(
+        (environment, sourceClass, out) -> out.print("\n\n\t\n"
+            + "  __  __ ______ _      _____                                                                   \n"
+            + " |  \\/  |  ____| |    |_   _|                                                                  \n"
+            + " | \\  / | |__  | |      | |                                                                    \n"
+            + " | |\\/| |  __| | |      | |                                                                    \n"
+            + " | |  | | |____| |____ _| |_                                                                   \n"
+            + " |_|__|_|______|______|_____|      ___ _ _ _                   _                       _       \n"
+            + " |_   _|               \\ \\        / (_) | (_)                 | |                     | |      \n"
+            + "   | |  _ __   __ _     \\ \\  /\\  / / _| | |_  __ _ _ __ ___   | |     ___ ______ _  __| | __ _ \n"
+            + "   | | | '_ \\ / _` |     \\ \\/  \\/ / | | | | |/ _` | '_ ` _ \\  | |    / _ \\_  / _` |/ _` |/ _` |\n"
+            + "  _| |_| | | | (_| |_     \\  /\\  /  | | | | | (_| | | | | | | | |___| (_) / / (_| | (_| | (_| |\n"
+            + " |_____|_| |_|\\__, (_)     \\/  \\/   |_|_|_|_|\\__,_|_| |_| |_| |______\\___/___\\__,_|\\__,_|\\__,_|\n"
+            + "               __/ |                                                                           \n"
+            + "              |___/                                                                            \n\n\n".toUpperCase()));
+    app.run(args);
+  }
 }
